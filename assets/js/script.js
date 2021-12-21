39247aaff548afca28817f9bac8df39f32f1049d
Tim <orcatim@hotmail.com>
2021-12-21 20:25:04 +0000

    add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,9 +48,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey, you got it right! :D ");
-
+        incrementScore();
     } else {
         alert(`Awww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -75,12 +76,25 @@ function calculateCorrectAnswer() {
 
 }
 
+
+/**
+ * gets the current score fromn the DOM and increments it
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+
+/**
+ * gets the tally of incorrect answers fromn the DOM and increments it
+ */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
