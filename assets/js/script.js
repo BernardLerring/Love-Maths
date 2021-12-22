bdd064f0575acfe0b8bae0c06892eac1ae769463
Tim <orcatim@hotmail.com>
2021-12-22 17:22:17 +0000

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
  * loaded and after the user's answer has been processed. 
  */
 
-function runGame(gameType) {
+ function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -30,7 +30,9 @@ function runGame(gameType) {
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
-        displayMultiplyQuestion(num1, num2);  
+        displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract" ) {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -73,6 +75,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"]
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"]
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"]
     } else {
         alert (`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}. Aborting!`;
@@ -111,7 +115,9 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
 
-    
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
